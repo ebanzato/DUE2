378344e984a6c03c4942420885b5b7712e43cf2b
ebanzato <50145503+ebanzato@users.noreply.github.com>
2025-10-22 11:43:42 +0200

    update find_neighbors

--- R/find_neighbors.R
+++ R/find_neighbors.R
@@ -2,25 +2,28 @@
 #'
 #' Finding neighbors of nodes in a graph.
 #'
-#' @param graph An igraph object.
+#' @param g An undirected graph represented as an igraph object.
 #'
-#' @return a list. Each element of the list corresponds to a node in the graph, with
-#' the corresponding neighboring nodes.
+#' @return a list. Each list element represents a graph node and contains its neighboring nodes.
 #'
 #' @examples
 #'
-#' g <- igraph::make_graph(c(1,2,2,3,3,1,3,4,4,5,5,6,6,3,6,4),directed=F)
+#' g <- igraph::make_graph(c(1,2,2,3,3,1,3,4,4,5,5,6,6,3,6,4), directed=F)
 #'
 #' ng <- find_neighbors(g)
 #'
 
-find_neighbors = function(graph){
+find_neighbors = function(g){
 
-  if(!igraph::is.igraph(graph)){
+  if(!igraph::is_igraph(g)){
     stop('Not an igraph object.')
   }
 
-  g_nodes = igraph::V(graph)
-  out = lapply(g_nodes, function(node) igraph::neighbors(graph, node))
+  if(igraph::is_directed(g)){
+    stop('Not an undirected graph.')
+  }
+
+  g_nodes = igraph::V(g)
+  out = lapply(g_nodes, function(node) igraph::neighbors(g, node))
   return(out)
 }
