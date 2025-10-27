d30927bea6a923d829168aa7edbdf89cdae14c86
ebanzato <50145503+ebanzato@users.noreply.github.com>
2025-10-27 14:29:29 +0100

    update size.factor

--- R/test_known.R
+++ R/test_known.R
@@ -13,6 +13,8 @@
 #'
 #' @param cell.group A vector of cell group labels, used as an extra covariate in the model and not for comparison, with length equal to the sample size.
 #'
+#' @param size.factor A vector of size factors with length equal to the sample size. It is used as an offset and to scale the covariates in the model.
+#'
 #' @param progressbar Logical, whether to display the progress bar. Default is TRUE.
 #'
 #' @return A list containing three elements: a matrix of p-values, a matrix of coefficients for the baseline group, and a matrix of coefficients for the differences between the two conditions.
@@ -31,7 +33,7 @@
 #' test.g <- test_known(g, g.df, g.gr, glm.family='poisson', cell.group=NULL, progressbar=TRUE)
 #'
 
-test_known = function(graph, data, group, glm.family, cell.group, progressbar){
+test_known = function(graph, data, group, glm.family, cell.group, size.factor, progressbar){
 
   if(glm.family=='quasipoisson'){
     test.aov = 'F'
@@ -46,93 +48,184 @@ test_known = function(graph, data, group, glm.family, cell.group, progressbar){
     pb = txtProgressBar(min = 0, max = length(n_nodes), style = 3)
   }
 
-  results = lapply(1:length(n_nodes), function(j) {
 
-    n_neigh = neigh[[j]]
+  if(is.null(size.factor)){
+    results = lapply(1:length(n_nodes), function(j) {
 
-    # out
-    res.p = rep(NA, length(n_nodes)+2) # pvalues
-    names(res.p) = c('LRT','group',n_nodes)
-    res.c = res.p[-1] # coef
-    res.b = res.c     # coef baseline
+      n_neigh = neigh[[j]]
 
-    if(length(n_neigh)==0){
+      # out
+      res.p = rep(NA, length(n_nodes)+2) # pvalues
+      names(res.p) = c('LRT','group',n_nodes)
+      res.c = res.p[-1] # coef
+      res.b = res.c     # coef baseline
 
-      if(is.null(cell.group)){
-        # define formula
-        formula_h0 = paste0(n_nodes[j], ' ~ 1')
-        formula_h1 = paste0(n_nodes[j], ' ~ group')
-        # models
-        mod_h0 = glm(formula_h0, data.frame(data,'group'=group), family=glm.family)  # model H0
-        mod_h1 = glm(formula_h1, data.frame(data,'group'=group), family=glm.family)  # model H1
-        cg = 0
+      if(length(n_neigh)==0){
+
+        if(is.null(cell.group)){
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ 1')
+          formula_h1 = paste0(n_nodes[j], ' ~ group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group), family=glm.family)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group), family=glm.family)  # model H1
+          cg = 0
+        }else{
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ cgroup')
+          formula_h1 = paste0(n_nodes[j], ' ~ cgroup + group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H1
+          cg = length(levels(as.factor(cell.group)))-1
+        }
+
       }else{
-        # define formula
-        formula_h0 = paste0(n_nodes[j], ' ~ cgroup')
-        formula_h1 = paste0(n_nodes[j], ' ~ cgroup + group')
-        # models
-        mod_h0 = glm(formula_h0, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H0
-        mod_h1 = glm(formula_h1, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H1
-        cg = length(levels(as.factor(cell.group)))-1
+
+        if(is.null(cell.group)){
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ ', paste(neigh[[j]], collapse='+', sep=''))
+          formula_h1 = paste0(n_nodes[j], ' ~ (', paste(neigh[[j]], collapse='+', sep=''),')*group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group), family=glm.family)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group), family=glm.family)  # model H1
+          cg = 0
+        }else{
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ cgroup +', paste(neigh[[j]], collapse='+', sep=''))
+          formula_h1 = paste0(n_nodes[j], ' ~ cgroup + (', paste(neigh[[j]], collapse='+', sep=''),')*group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H1
+          cg = length(levels(as.factor(cell.group)))-1
+        }
       }
 
-    }else{
-
-      if(is.null(cell.group)){
-        # define formula
-        formula_h0 = paste0(n_nodes[j], ' ~ ', paste(neigh[[j]], collapse='+', sep=''))
-        formula_h1 = paste0(n_nodes[j], ' ~ (', paste(neigh[[j]], collapse='+', sep=''),')*group')
-        # models
-        mod_h0 = glm(formula_h0, data.frame(data,'group'=group), family=glm.family)  # model H0
-        mod_h1 = glm(formula_h1, data.frame(data,'group'=group), family=glm.family)  # model H1
-        cg = 0
-      }else{
-        # define formula
-        formula_h0 = paste0(n_nodes[j], ' ~ cgroup +', paste(neigh[[j]], collapse='+', sep=''))
-        formula_h1 = paste0(n_nodes[j], ' ~ cgroup + (', paste(neigh[[j]], collapse='+', sep=''),')*group')
-        # models
-        mod_h0 = glm(formula_h0, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H0
-        mod_h1 = glm(formula_h1, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family)  # model H1
-        cg = length(levels(as.factor(cell.group)))-1
+      # Test
+      test = anova(mod_h0, mod_h1, test=test.aov)
+      if(test.aov=='Chisq'){
+        res.p[1] = test$`Pr(>Chi)`[2]
       }
+      if(test.aov=='F'){
+        res.p[1] = test$`Pr(>F)`[2]
+      }
+      pvalbeta = summary(mod_h1)$coefficients[-c(1:(1+cg+length(n_neigh))),4]
+      # out
+      res.p[2] = pvalbeta[1]
+      res.p[-c(1:2)][which(n_nodes %in% neigh[[j]])] = pvalbeta[-1]
+
+      # Coef delta
+      coefdelta = summary(mod_h1)$coefficients[-c(1:(1+cg+length(n_neigh))),1]
+      # out
+      res.c[1] = coefdelta[1]
+      res.c[-1][which(n_nodes %in% neigh[[j]])] = coefdelta[-1]
+
+      # Coef baseline (ref)
+      coefbase = summary(mod_h1)$coefficients[c(1,(2+cg):(1+cg+length(n_neigh))),1]
+      # out
+      res.b[1] = coefbase[1]
+      res.b[-1][which(n_nodes %in% neigh[[j]])] = coefbase[-1]
+
+
+
+      if(progressbar==TRUE){
+        setTxtProgressBar(pb, j)
+      }
+
+      # OUT
+      res = list('p.mat'=res.p,'c.mat'=res.c, 'b.mat'=res.b)
+      return(res)
     }
+    )
+  }else{
+    results = lapply(1:length(n_nodes), function(j) {
 
-    # Test
-    test = anova(mod_h0, mod_h1, test=test.aov)
-    if(test.aov=='Chisq'){
-      res.p[1] = test$`Pr(>Chi)`[2]
+      n_neigh = neigh[[j]]
+
+      # out
+      res.p = rep(NA, length(n_nodes)+2) # pvalues
+      names(res.p) = c('LRT','group',n_nodes)
+      res.c = res.p[-1] # coef
+      res.b = res.c     # coef baseline
+
+      if(length(n_neigh)==0){
+
+        if(is.null(cell.group)){
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ 1')
+          formula_h1 = paste0(n_nodes[j], ' ~ group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group), family=glm.family, offset=size.factor)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group), family=glm.family, offset=size.factor)  # model H1
+          cg = 0
+        }else{
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ cgroup')
+          formula_h1 = paste0(n_nodes[j], ' ~ cgroup + group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family, offset=size.factor)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family, offset=size.factor)  # model H1
+          cg = length(levels(as.factor(cell.group)))-1
+        }
+
+      }else{
+
+        if(is.null(cell.group)){
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ ', paste(sprintf("I(%s/size.factor)", neigh[[j]]), collapse='+', sep=''))
+          formula_h1 = paste0(n_nodes[j], ' ~ (', paste(sprintf("I(%s/size.factor)", neigh[[j]]), collapse='+', sep=''),')*group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group), family=glm.family, offset=size.factor)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group), family=glm.family, offset=size.factor)  # model H1
+          cg = 0
+        }else{
+          # define formula
+          formula_h0 = paste0(n_nodes[j], ' ~ cgroup +', paste(sprintf("I(%s/size.factor)", neigh[[j]]), collapse='+', sep=''))
+          formula_h1 = paste0(n_nodes[j], ' ~ cgroup + (', paste(sprintf("I(%s/size.factor)", neigh[[j]]), collapse='+', sep=''),')*group')
+          # models
+          mod_h0 = glm(formula_h0, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family, offset=size.factor)  # model H0
+          mod_h1 = glm(formula_h1, data.frame(data,'group'=group,'cgroup'=cell.group), family=glm.family, offset=size.factor)  # model H1
+          cg = length(levels(as.factor(cell.group)))-1
+        }
+      }
+
+      # Test
+      test = anova(mod_h0, mod_h1, test=test.aov)
+      if(test.aov=='Chisq'){
+        res.p[1] = test$`Pr(>Chi)`[2]
+      }
+      if(test.aov=='F'){
+        res.p[1] = test$`Pr(>F)`[2]
+      }
+      pvalbeta = summary(mod_h1)$coefficients[-c(1:(1+cg+length(n_neigh))),4]
+      # out
+      res.p[2] = pvalbeta[1]
+      res.p[-c(1:2)][which(n_nodes %in% neigh[[j]])] = pvalbeta[-1]
+
+      # Coef delta
+      coefdelta = summary(mod_h1)$coefficients[-c(1:(1+cg+length(n_neigh))),1]
+      # out
+      res.c[1] = coefdelta[1]
+      res.c[-1][which(n_nodes %in% neigh[[j]])] = coefdelta[-1]
+
+      # Coef baseline (ref)
+      coefbase = summary(mod_h1)$coefficients[c(1,(2+cg):(1+cg+length(n_neigh))),1]
+      # out
+      res.b[1] = coefbase[1]
+      res.b[-1][which(n_nodes %in% neigh[[j]])] = coefbase[-1]
+
+
+
+      if(progressbar==TRUE){
+        setTxtProgressBar(pb, j)
+      }
+
+      # OUT
+      res = list('p.mat'=res.p,'c.mat'=res.c, 'b.mat'=res.b)
+      return(res)
     }
-    if(test.aov=='F'){
-      res.p[1] = test$`Pr(>F)`[2]
-    }
-    pvalbeta = summary(mod_h1)$coefficients[-c(1:(1+cg+length(n_neigh))),4]
-    # out
-    res.p[2] = pvalbeta[1]
-    res.p[-c(1:2)][which(n_nodes %in% neigh[[j]])] = pvalbeta[-1]
-
-    # Coef delta
-    coefdelta = summary(mod_h1)$coefficients[-c(1:(1+cg+length(n_neigh))),1]
-    # out
-    res.c[1] = coefdelta[1]
-    res.c[-1][which(n_nodes %in% neigh[[j]])] = coefdelta[-1]
-
-    # Coef baseline (ref)
-    coefbase = summary(mod_h1)$coefficients[c(1,(2+cg):(1+cg+length(n_neigh))),1]
-    # out
-    res.b[1] = coefbase[1]
-    res.b[-1][which(n_nodes %in% neigh[[j]])] = coefbase[-1]
-
-
-
-    if(progressbar==TRUE){
-      setTxtProgressBar(pb, j)
-    }
-
-    # OUT
-    res = list('p.mat'=res.p,'c.mat'=res.c, 'b.mat'=res.b)
-    return(res)
+    )
   }
-  )
 
   out.p = t(sapply(results, function(x) c(x$p.mat)))
   rownames(out.p) = n_nodes
